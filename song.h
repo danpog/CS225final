9cf3231bb8a30162d744ffd15f85604f4d59e700
Edan Elazar <edan.y.elazar@gmail.com>
2022-11-20 05:23:43 +0000

    GetArtist() function

--- song.h
+++ song.h
@@ -7,6 +7,7 @@ class Song  {
         Song() = default;
         Song(std::string name, std::string album, std::string artist);
         std::string toString() const;
+        std::string GetArtist() const;
         friend bool operator==(const Song& LHS, const Song& RHS);
         friend ostream & operator <<(ostream &out, const Song& s);
         friend bool operator!=(const Song& LHS, const Song& RHS);
