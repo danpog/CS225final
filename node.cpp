65677e532a43c4f608e5ad27dffe852845ab4c0d
Eli Gidron <egidron2@illinois.edu>
2022-11-20 20:46:36 +0000

    node operators and binary search

--- node.cpp
+++ node.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -21,7 +22,14 @@ void Node::AddSong(Song& song)    {
     _popular_songs.push_back(song);
 }
 void Node::AddNeighbor(Node* node)   {
-    bool found = false;
+    int i = AddNeighborRecursive(node, 0, _neighbors.size() - 1);
+    if (i >= 0)  {
+        _neighbors.insert(_neighbors.begin() + i, std::pair<Node*, double>(node, 1));
+    }
+    
+
+    /*bool found = false;
+    
     for (std::pair<Node*, double>& p : _neighbors)  {
         if (p.first == node)    {
             p.second++;
@@ -31,8 +39,27 @@ void Node::AddNeighbor(Node* node)   {
     }
     if (!found) {
         _neighbors.push_back(std::pair<Node*, double>(node, 1));
+    }*/
+    //43 s
+}
+
+int Node::AddNeighborRecursive(Node* node, int min, int max)   {
+    if (min > max)  {
+        return min;
+    }
+    int mid = (min + max)/2;
+    if (_neighbors[mid].first == node) {
+        _neighbors[mid].second++;
+        return -1;
+    }
+    else if (*_neighbors[mid].first < *node)    {
+        return AddNeighborRecursive(node, mid + 1, max);
+    }
+    else    {
+        return AddNeighborRecursive(node, min, mid - 1);
     }
 }
+
 double Node::GetWeight(Node* node)  {
     for (std::pair<Node*, double>& p : _neighbors)  {
         if (p.first == node)    {
@@ -48,4 +75,17 @@ double Node::GetWeight(string artist)   {
         }
     }
     return 0;
+}
+
+bool operator<(Node& LHS, Node& RHS)    {
+    return LHS.GetArtist() < RHS.GetArtist();
+}
+bool operator==(Node& LHS, Node& RHS)   {
+    return LHS.GetArtist() == RHS.GetArtist();
+}
+bool operator!=(Node& LHS, Node& RHS)   {
+    return !(LHS == RHS);
+}
+bool operator<=(Node& LHS, Node& RHS)   {
+    return LHS < RHS || LHS == RHS;
 }
--- node.h
+++ node.h
@@ -18,10 +18,15 @@ class Node  {
         void AddNeighbor(Node* node);
         double GetWeight(Node* node);
         double GetWeight(string artist);
+        friend bool operator<(Node& LHS, Node& RHS);
+        friend bool operator==(Node& LHS, Node& RHS);
+        friend bool operator!=(Node& LHS, Node& RHS);
+        friend bool operator<=(Node& LHS, Node& RHS);
 
     private:
         string _artist;
         vector<Song> _popular_songs;
         size_t _position = 0;
         vector<std::pair<Node*, double>> _neighbors;
+        int AddNeighborRecursive(Node* node, int min, int max);
 };
