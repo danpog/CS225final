9c57f0443dc29bf2dbbfd18ede6742a4078b6cc9
Daniel Pogrebinsky <dgp4@illinois.edu>
2022-11-29 01:31:33 +0000

    More testing

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,6 +2,11 @@
 #  define CATCH_CONFIG_MAIN
 #endif
 
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+//                                 Includes //
+/////////////////////////////////////////////////////////////////////////////////////////////
+
 #include <catch/catch.hpp>
 #include "../node.cpp"
 #include "../playlist.cpp"
@@ -9,6 +14,9 @@
 #include "../graph.cpp"
 #include <json/json.h>
 
+/////////////////////////////////////////////////////////////////////////////////////////////
+//                             Helpers/Constants //
+/////////////////////////////////////////////////////////////////////////////////////////////
 
 vector<Playlist> parse(string filename)    {
     if (filename.empty()) {
@@ -50,6 +58,10 @@ bool NeighborsContain(Node* node, Node* target, double count)   {
     return node->GetNeighbors()[target] == count;
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////
+//                                Test Cases //
+/////////////////////////////////////////////////////////////////////////////////////////////
+
 TEST_CASE("Check Song Equality") { 
     Song s = Song("a", "b", "c", "d");
     Song t = Song("a", "b", "c", "e");
@@ -88,18 +100,12 @@ TEST_CASE("Check Playlist") {
     a = parse("./old_testing/SongsLimit.json");
     Graph graph = Graph(a);
     for (auto x: graph.getGraph()) {
-        graph.GetNode(x.first)->TrimNeighbors(3, true);
+        graph.GetNode(x.first)->TrimNeighbors(3, false);
     }
     vector<Song> song_prefs;
     song_prefs.push_back(graph.GetNode("\"Missy Elliott\"") -> FindSong("\"Lose Control (feat. Ciara & Fat Man Scoop)\""));
     Playlist p = graph.CreatePlaylist(9, song_prefs);
 
-    /*Node* source = graph.GetNode("\"Missy Elliott\"");
-    Node* top = graph.GetNode("\"Jesse McCartney\"");
-    Node* second = graph.GetNode("\"Chris Brown\"");
-    Node* third = graph.GetNode("\"Justin Bieber\"");
-    Node* bieber3 = graph.GetNode("\"Destiny's Child\"");*/
-    //7-9
     REQUIRE(p.GetSongs().size() == 9);
     for (auto i : p.GetSongs()) {
         std::cout << i._name << std::endl;
@@ -109,6 +115,62 @@ TEST_CASE("Check Playlist") {
     REQUIRE(p.ContainsSongByName("\"Run It!\""));
 
     REQUIRE(p.ContainsSongByName("\"Yo (Excuse Me Miss)\""));
-    REQUIRE(p.ContainsSongByName("\"Say My Name\""));
     REQUIRE(p.ContainsSongByName("\"Somebody To Love\""));
+    // REQUIRE(p.ContainsSongByName("\"Buttons\""));
 }
+
+TEST_CASE("Check playlist larger") {
+    vector<Playlist> a;
+
+    a = parse("./old_testing/Songs0-600.json");
+    Graph graph = Graph(a);
+    for (auto x: graph.getGraph()) {
+        graph.GetNode(x.first)->TrimNeighbors(3, false);
+    }
+    vector<Song> song_prefs;
+    song_prefs.push_back(graph.GetNode("\"Missy Elliott\"") -> FindSong("\"Lose Control (feat. Ciara & Fat Man Scoop)\""));
+    Playlist p = graph.CreatePlaylist(9, song_prefs);
+
+    REQUIRE(p.GetSongs().size() == 9);
+    for (auto i : p.GetSongs()) {
+        std::cout << i._name << std::endl;
+    }
+
+    REQUIRE(p.ContainsSongByName("\"Work It\"")); 
+    REQUIRE(p.ContainsSongByName("\"Ni**as In Paris\""));
+    REQUIRE(p.ContainsSongByName("\"One Dance\"")); 
+}   
+
+TEST_CASE("Check playlist larger2") {
+    Song q = Song("a1", "b", "1", "A");
+    Song r = Song("a2", "b", "2", "B");
+    Song s = Song("a3", "b", "3", "C");
+    Song t = Song("a4", "b", "4", "D");
+    Song u = Song("a5", "b", "5", "E");
+    Song v = Song("a6", "b", "6", "F");
+    std::vector<Song> songs = {q, r, s, t, u, v};
+    
+    Graph g;  
+    auto _graph = g.getGraph();
+    _graph.insert(pair<string, Node>("1", Node("1")));
+    _graph.insert(pair<string, Node>("2", Node("2")));
+    _graph.insert(pair<string, Node>("3", Node("3")));
+    _graph.insert(pair<string, Node>("4", Node("4")));
+    _graph.insert(pair<string, Node>("5", Node("5")));
+    _graph.insert(pair<string, Node>("6", Node("6")));
+
+    _graph["1"].AddNeighbor(&_graph["2"]);
+    _graph["2"].AddNeighbor(&_graph["3"]);
+    _graph["3"].AddNeighbor(&_graph["4"]);
+    _graph["4"].AddNeighbor(&_graph["5"]);
+    _graph["4"].AddNeighbor(&_graph["6"]);
+
+    Playlist p = g.CreatePlaylist(9, songs);
+    for (auto i : p.GetSongs()) {
+        std::cout << i._name << std::endl;
+    }    
+
+}
+
+
+
