81ed9ec2c5c1d4f629a948021301c39443694df5
Eli Gidron <egidron2@illinois.edu>
2022-12-11 01:35:16 +0000

    similarity test cases

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -21,11 +21,45 @@ bool NeighborsContain(Node* node, Node* target, double count)   {
     return node->GetNeighbors()[target] == count;
 }
 
+Graph CustomGraph(std::vector<Song> input_songs)    {
+    Song q = input_songs[0];
+    Song r = input_songs[1];
+    Song s = input_songs[2];
+    Song t = input_songs[3];
+    Song u = input_songs[4];
+    Song v = input_songs[5];
+    std::vector<Song> songs = {q};
+    Graph g;  
+    unordered_map<string, Node>& _graph = g.getGraph();
+    _graph.insert(pair<string, Node>("1", Node("1")));
+    _graph.insert(pair<string, Node>("2", Node("2")));
+    _graph.insert(pair<string, Node>("3", Node("3")));
+    _graph.insert(pair<string, Node>("4", Node("4")));
+    _graph.insert(pair<string, Node>("5", Node("5")));
+    _graph.insert(pair<string, Node>("6", Node("6")));
+
+
+    _graph["1"].AddNeighbor(&_graph["2"]);
+    _graph["2"].AddNeighbor(&_graph["3"]);
+    _graph["3"].AddNeighbor(&_graph["4"]);
+    _graph["4"].AddNeighbor(&_graph["5"]);
+    _graph["4"].AddNeighbor(&_graph["5"]);
+    _graph["4"].AddNeighbor(&_graph["6"]);
+
+    g.GetNode("1")->AddSongPair(q, 1);
+    g.GetNode("2")->AddSongPair(r, 1);
+    g.GetNode("3")->AddSongPair(s, 1);
+    g.GetNode("4")->AddSongPair(t, 1);
+    g.GetNode("5")->AddSongPair(u, 1);
+    g.GetNode("6")->AddSongPair(v, 1);
+    return g;
+}
+
 /////////////////////////////////////////////////////////////////////////////////////////////
 //                                Test Cases                                               //
 /////////////////////////////////////////////////////////////////////////////////////////////
 
-TEST_CASE("Check Song Equality") { 
+TEST_CASE("Song Equality") { 
     Song s = Song("a", "b", "c", "d");
     Song t = Song("a", "b", "c", "e");
     Song u = Song("a", "b", "c", "e");
@@ -34,7 +68,7 @@ TEST_CASE("Check Song Equality") {
     REQUIRE_FALSE(s == t);
 }
 
-TEST_CASE("Check Request Song") {
+TEST_CASE("Request Song") {
     Node n("artist");
     Song q = Song("a1", "b", "artist", "A");
     Song r = Song("a2", "b", "artist", "B");
@@ -56,7 +90,7 @@ TEST_CASE("Check Request Song") {
     REQUIRE(n.RequestSong(50) == q);
 }
 
-TEST_CASE("Check Neighbors") { 
+TEST_CASE("Trimmed Neighbors") { 
     vector<Playlist> a;
     
     //a = parse("./tests/old_testing/SongsLimit.json");
@@ -81,7 +115,7 @@ TEST_CASE("Check Neighbors") {
     REQUIRE(NeighborsContain(third, second, 4));
 }
 
-TEST_CASE("Check Playlist") { 
+TEST_CASE("Small Playlist") { 
     vector<Playlist> a;
     
     Graph graph;
@@ -107,7 +141,7 @@ TEST_CASE("Check Playlist") {
 
 }
 
-TEST_CASE("Check playlist larger") {
+TEST_CASE("Large Playlist") {
     vector<Playlist> a;
     
     Graph graph;
@@ -128,35 +162,16 @@ TEST_CASE("Check playlist larger") {
     REQUIRE(p.ContainsSongByName("\"Forever\""));
 }   
 
-TEST_CASE("Check playlist constructed") {
+TEST_CASE("Constructed Graph Playlist") {
     Song q = Song("a1", "b", "1", "A");
     Song r = Song("a2", "b", "2", "B");
     Song s = Song("a3", "b", "3", "C");
     Song t = Song("a4", "b", "4", "D");
     Song u = Song("a5", "b", "5", "E");
     Song v = Song("a6", "b", "6", "F");
+    std::vector<Song> input_songs = {q, r, s, t, u, v};
+    Graph g = CustomGraph(input_songs);
     std::vector<Song> songs = {q};
-    Graph g;  
-    unordered_map<string, Node>& _graph = g.getGraph();
-    _graph.insert(pair<string, Node>("1", Node("1")));
-    _graph.insert(pair<string, Node>("2", Node("2")));
-    _graph.insert(pair<string, Node>("3", Node("3")));
-    _graph.insert(pair<string, Node>("4", Node("4")));
-    _graph.insert(pair<string, Node>("5", Node("5")));
-    _graph.insert(pair<string, Node>("6", Node("6")));
-
-    _graph["1"].AddNeighborPair(std::pair<Node*, int>(&_graph["2"], 1));
-    _graph["2"].AddNeighborPair(std::pair<Node*, int>(&_graph["3"], 1));
-    _graph["3"].AddNeighborPair(std::pair<Node*, int>(&_graph["4"], 1));
-    _graph["4"].AddNeighborPair(std::pair<Node*, int>(&_graph["5"], 1));
-    _graph["4"].AddNeighborPair(std::pair<Node*, int>(&_graph["6"], 1));
-
-    g.GetNode("1")->AddSongPair(q, 1);
-    g.GetNode("2")->AddSongPair(r, 1);
-    g.GetNode("3")->AddSongPair(s, 1);
-    g.GetNode("4")->AddSongPair(t, 1);
-    g.GetNode("5")->AddSongPair(u, 1);
-    g.GetNode("6")->AddSongPair(v, 1);
 
     Playlist p = g.CreatePlaylist(1, songs);
     REQUIRE(p.GetSongs().size() == 1);
@@ -188,4 +203,72 @@ TEST_CASE("Check playlist constructed") {
     REQUIRE(p5.ContainsSongByName("a4"));
     REQUIRE(p5.ContainsSongByName("a5"));
     REQUIRE(p5.ContainsSongByName("a6"));   
+}
+
+TEST_CASE("Constructed Graph Similarity")   {
+    Song q = Song("a1", "b", "1", "A");
+    Song r = Song("a2", "b", "2", "B");
+    Song s = Song("a3", "b", "3", "C");
+    Song t = Song("a4", "b", "4", "D");
+    Song u = Song("a5", "b", "5", "E");
+    Song v = Song("a6", "b", "6", "F");
+    std::vector<Song> input_songs = {q, r, s, t, u, v};
+    Graph g = CustomGraph(input_songs);
+
+    REQUIRE(g.similarity("1", "2", 1));
+    REQUIRE(g.similarity("1", "2", 2));
+    REQUIRE(g.similarity("1", "4", 3));
+    REQUIRE(g.similarity("1", "5", 4));
+    REQUIRE(g.similarity("1", "6", 4));
+    REQUIRE(g.similarity("2", "6", 3));
+    REQUIRE(g.similarity("3", "6", 2));
+    REQUIRE_FALSE(g.similarity("1", "4", 2));
+    REQUIRE_FALSE(g.similarity("2", "6", 2));
+    REQUIRE_FALSE(g.similarity("1", "2", 0));
+}
+
+TEST_CASE("Graph Similarity")   {
+   vector<Playlist> a;
+    
+    Graph graph;
+    a = ParseCSV("../tests/test_playlist_large.csv");
+    graph.analyze_all_playlists(a);
+    for (auto x: graph.getGraph()) {
+        graph.GetNode(x.first)->TrimNeighbors(3, true);
+    }
+    
+    REQUIRE(graph.similarity("\"Missy Elliott\"", "\"Justin Bieber\"", 1));
+    REQUIRE(graph.similarity("\"Missy Elliott\"", "\"The Pussycat Dolls\"", 2));
+    REQUIRE(graph.similarity("\"Missy Elliott\"", "\"The Black Eyed Peas\"", 2));
+    REQUIRE_FALSE(graph.similarity("\"Missy Elliott\"", "\"The Black Eyed Peas\"", 1));
+    REQUIRE_FALSE(graph.similarity("\"Missy Elliott\"", "\"Flo Rida\"", 1));
+}
+
+TEST_CASE("Constructed Graph Give Me Something New")  {
+    Song q = Song("a1", "b", "1", "A");
+    Song r = Song("a2", "b", "2", "B");
+    Song s = Song("a3", "b", "3", "C");
+    Song t = Song("a4", "b", "4", "D");
+    Song u = Song("a5", "b", "5", "E");
+    Song v = Song("a6", "b", "6", "F");
+    std::vector<Song> input_songs = {q, r, s, t, u, v};
+    Graph g = CustomGraph(input_songs);
+
+    REQUIRE(g.somethingNew("1", 3) == "4");
+    REQUIRE(g.somethingNew("1", 4) == "5");
+    REQUIRE((g.somethingNew("2", 1) == "1" || g.somethingNew("2", 1) == "3"));
+}
+
+TEST_CASE("Give Me Something New")  {
+    /*vector<Playlist> a;
+    Graph graph;
+    a = ParseCSV("../csvdata/mpd.slice.0-999.csv");
+    graph.analyze_all_playlists(a);
+    for (auto x: graph.getGraph()) {
+        graph.GetNode(x.first)->TrimNeighbors(10, true);
+    }
+    cout << graph.somethingNew("\"Missy Elliott\"", 1) << endl;
+    cout << graph.somethingNew("\"Missy Elliott\"", 2) << endl;
+    cout << graph.somethingNew("\"Missy Elliott\"", 3) << endl;
+    cout << graph.somethingNew("\"Missy Elliott\"", 4) << endl;*/
 }
